c4bc20c8a4445cae124a99e3cd3a34699b8858de
Alessandro Polastri <a.polastri@campus.unimib.it>
2017-12-20 10:59:30 +0100

    Crazione del Progetto e primo commit

---
+++ app/src/main/java/polastri/alessandro/agendapersonale/In_Corso.java
@@ -0,0 +1,13 @@
+package polastri.alessandro.agendapersonale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class In_Corso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in__corso);
+    }
+}
