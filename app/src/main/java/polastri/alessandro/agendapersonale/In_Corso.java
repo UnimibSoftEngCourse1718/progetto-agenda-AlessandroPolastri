c55a9a96555cf5208767478c6b0d938c3cca2067
Alessandro Polastri <a.polastri@campus.unimib.it>
2017-12-20 17:21:35 +0100

    Altro Test

--- app/src/main/java/polastri/alessandro/agendapersonale/In_Corso.java
+++ app/src/main/java/polastri/alessandro/agendapersonale/In_Corso.java
@@ -9,5 +9,7 @@ public class In_Corso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in__corso);
+
     }
+
 }
