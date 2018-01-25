f72da97a4c9c70ea36b5ef143ad5474cd52bdc24
Alessandro Polastri <a.polastri@campus.unimib.it>
2018-01-25 20:43:22 +0100

    Aggiustata un po di grafica per Impegni in corso

--- app/src/main/java/polastri/alessandro/agendapersonale/polastri/alessandro/agendapersonale/impegni/ImpegniInCorso.java
+++ app/src/main/java/polastri/alessandro/agendapersonale/polastri/alessandro/agendapersonale/impegni/ImpegniInCorso.java
@@ -22,7 +22,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import polastri.alessandro.agendapersonale.R;
-import polastri.alessandro.agendapersonale.polastri.alessandro.agendapersonale.rubrica.Rubrica;
 
 public class ImpegniInCorso extends AppCompatActivity {
 
